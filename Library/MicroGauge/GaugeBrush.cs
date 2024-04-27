016de110134ef199cb2dd9df3e9637513579465f
Galen Blakeman <galenblakeman@gmail.com>
2024-04-27 09:22:52 -0500

    Add GaugeBrush to provide cross platform brush that supports linear gradient shaders.

---
+++ Library/MicroGauge/GaugeBrush.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using SkiaSharp;
+// ReSharper disable UnusedMember.Global
+
+namespace MicroGauge
+{
+
+    public class GaugeBrush
+    {
+        public SKPoint StartPoint { get; set; } = new SKPoint(0, 0);
+        public SKPoint EndPoint { get; set; } = new SKPoint(1, 0);
+        public List<GaugeBrushStop> GradientStops { get; set; } = new List<GaugeBrushStop>();
+
+        /// <summary>
+        ///     Constructor - Empty
+        /// </summary>
+        public GaugeBrush()
+        {
+
+        }
+
+        /// <summary>
+        ///     Constructor - Start & End Points
+        /// </summary>
+        public GaugeBrush(SKPoint startPoint, SKPoint endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        /// <summary>
+        ///     Constructor - Solid Color
+        /// </summary>
+        public GaugeBrush(SKColor color)
+        {
+            GradientStops = new List<GaugeBrushStop>
+            {
+                new GaugeBrushStop(color, 0),
+                new GaugeBrushStop(color, 1)
+            };
+        }
+
+        /// <summary>
+        /// 	Constructor - List Gradient Stops
+        /// </summary>
+        public GaugeBrush(List<GaugeBrushStop> gradientStops)
+        {
+            GradientStops = gradientStops;
+        }
+
+        /// <summary>
+        /// 	AddStop - Chainable Add Gradient Stop
+        /// </summary>
+        public GaugeBrush AddStop(GaugeBrushStop gradientStop)
+        {
+            GradientStops.Add(gradientStop);
+            return this;
+        }
+    }
+}
---
+++ Library/MicroGauge/GaugeBrushStop.cs
@@ -0,0 +1,27 @@
+﻿using SkiaSharp;
+
+namespace MicroGauge
+{
+    public class GaugeBrushStop
+    {
+
+        public SKColor Color { get; set; }
+        public float Offset { get; set; }
+
+        /// <summary>
+        ///     Constructor - Empty
+        /// </summary>
+        public GaugeBrushStop()
+        {
+        }
+
+        /// <summary>
+        ///     Constructor - color offset
+        /// </summary>
+        public GaugeBrushStop(SKColor color, float offset)
+        {
+            Color = color;
+            Offset = offset;
+        }
+    }
+}
