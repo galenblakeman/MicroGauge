68da0bb5380288eee79e372e0146a983782fd675
Galen Blakeman <galenblakeman@gmail.com>
2024-04-27 14:35:32 -0500

    Remove old shader helpers

--- Library/MicroGauge.Forms/XfGaugeHelper.cs
+++ Library/MicroGauge.Forms/XfGaugeHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
@@ -37,46 +36,6 @@ namespace MicroGauge.Forms
                 brush.AddStop(new GaugeBrushStop(stop.Color.ToSKColor(), stop.Offset));
             }
             return brush;
-        }
-
-
-        /// <summary>
-        ///     GetSkShader
-        /// </summary>
-        public static SKShader GetSkShader(Brush sourceBrush,
-            SKPoint offset, float width, float height)
-        {
-            switch (sourceBrush)
-            {
-                case SolidColorBrush solidColorBrush:
-                    return SKShader.CreateColor(solidColorBrush.Color.ToSKColor());
-                case LinearGradientBrush linearGradientBrush:
-                    return ConvertToSkiaLinearGradient(linearGradientBrush, offset, width, height);
-                default:
-                    return SKShader.CreateColor(SKColors.Transparent);
-            }
-        }
-
-        /// <summary>
-        ///     ConvertToSkiaLinearGradient
-        /// </summary>
-        private static SKShader ConvertToSkiaLinearGradient(LinearGradientBrush platformBrush,
-            SKPoint offset, float width, float height)
-        {
-            var startX = offset.X + platformBrush.StartPoint.X * width;
-            var startY = offset.Y + platformBrush.StartPoint.Y * height;
-            var endX = offset.X + platformBrush.EndPoint.X * width;
-            var endY = offset.Y + platformBrush.EndPoint.Y * height;
-
-            var colors = platformBrush.GradientStops.Select(stop => stop.Color.ToSKColor()).ToArray();
-            var positions = platformBrush.GradientStops.Select(stop => stop.Offset).ToArray();
-
-            return SKShader.CreateLinearGradient(
-                new SKPoint(Convert.ToSingle(startX), Convert.ToSingle(startY)),
-                new SKPoint(Convert.ToSingle(endX), Convert.ToSingle(endY)),
-                colors,
-                positions,
-                SKShaderTileMode.Clamp);
         }
     }
 }
--- Library/MicroGauge.Maui/MauiGaugeHelper.cs
+++ Library/MicroGauge.Maui/MauiGaugeHelper.cs
@@ -33,41 +33,4 @@ public static class MauiGaugeHelper
         }
         return brush;
     }
-
-    /// <summary>
-    ///     GetSkShader
-    /// </summary>
-    public static SKShader GetSkShader(Brush sourceBrush,
-        SKPoint offset, float width, float height)
-    {
-        return sourceBrush switch
-        {
-            SolidColorBrush solidColorBrush => SKShader.CreateColor(solidColorBrush.Color.ToSKColor()),
-            LinearGradientBrush linearGradientBrush => ConvertToSkiaLinearGradient(linearGradientBrush,
-                offset, width, height),
-            _ => SKShader.CreateColor(SKColors.Transparent)
-        };
-    }
-
-    /// <summary>
-    ///     ConvertToSkiaLinearGradient
-    /// </summary>
-    private static SKShader ConvertToSkiaLinearGradient(LinearGradientBrush platformBrush,
-        SKPoint offset, float width, float height)
-    {
-        var startX = offset.X + platformBrush.StartPoint.X * width;
-        var startY = offset.Y + platformBrush.StartPoint.Y * height;
-        var endX = offset.X + platformBrush.EndPoint.X * width;
-        var endY = offset.Y + platformBrush.EndPoint.Y * height;
-
-        var colors = platformBrush.GradientStops.Select(stop => stop.Color.ToSKColor()).ToArray();
-        var positions = platformBrush.GradientStops.Select(stop => stop.Offset).ToArray();
-
-        return SKShader.CreateLinearGradient(
-            new SKPoint(Convert.ToSingle(startX), Convert.ToSingle(startY)),
-            new SKPoint(Convert.ToSingle(endX), Convert.ToSingle(endY)),
-            colors,
-            positions,
-            SKShaderTileMode.Clamp);
-    }
 }
